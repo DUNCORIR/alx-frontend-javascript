01fbd86b65772e3fc8176cbc7e6ecb16b2d676da
duncorir <duncorir@mail.com>
2025-04-16 13:04:02 +0300

    Final User

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUp = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUp, upload])
-    .then((results) =>
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason.message,
-      }))
-    );
+  return Promise.allSettled([signUp, upload]);
 }
