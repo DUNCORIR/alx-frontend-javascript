5d340a8209144531ba468caec35d7e8364368844
duncorir <duncorir@mail.com>
2025-04-16 13:08:04 +0300

    Await/Assunc

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -5,8 +5,15 @@ export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    return { photo, user };
+
+   return {
+      photo,
+      user,
+    };
   } catch (error) {
-    return { photo: null, user: null };
+    return {
+      photo: null,
+      user: null,
+    };
   }
 }
