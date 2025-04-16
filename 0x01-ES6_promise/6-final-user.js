df219fe093aa3721cee2fcd19522de45c64ecacd
duncorir <duncorir@mail.com>
2025-04-16 13:01:52 +0300

    Final User

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signupPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
 
-  return Promise.allSettled([signupPromise, photoPromise])
+  return Promise.allSettled([signUp, upload])
     .then((results) =>
-      results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: result.status,
-            value: result.value,
-          };
-        }
-        return {
-          status: result.status,
-          value: result.reason.message,
-        };
-      })
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason.message,
+      }))
     );
 }
